65593f588fd4a25ae35c7eff65e9cf93de8f666c
ro-mak <eee127@yandex.ru>
2019-03-23 15:31:20 +0300

    -corrected gitignore

---
+++ src/SensorTemperature.java
@@ -0,0 +1,7 @@
+public interface SensorTemperature {
+    int identifier(); // идентификатор датчика
+    double temperature(); // температура датчика
+    int year();          // Год
+    int day();           // День года
+    int second();        // Секунда дня
+}
